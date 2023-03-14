0cccaff8651ab1ff055d02389abee383d50fd00c
cstrayer24 <cstrayer4303@gmail.com>
2023-03-14 19:57:31 -0400

    tues mar 14

--- components/mainTwitter/Tweet.tsx
+++ components/mainTwitter/Tweet.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 //local code ⬇️
 import Layout from "../layout";
+import Heart from "../icons/heart";
 let txt;
 
 function Tweet(props): JSX.Element {
@@ -14,17 +15,23 @@ function Tweet(props): JSX.Element {
     const { data, error, isLoading } = useSwr(`api/send-tweets`, fetcher);
     return { data, error, isLoading };
   };
-  const tweetArr = tweets();
-  // console.log(tweetArr.data.length);
+  const allTweets = tweets();
+
   const { data, error, isLoading } = useSwr(
     `api/postData/${Math.floor(Math.random() * 3)}`,
     fetcher
   );
-
   useEffect(() => {
     data;
   }, []);
-
+  async function like() {
+    const req = await fetch("/api/like", {
+      method: "POST",
+      body: JSON.stringify({ body: data.body }),
+    });
+    const res = await req.json();
+    console.log(res);
+  }
   if (error) return <div>failed</div>;
   if (isLoading) return <div>loading...</div>;
   return (
@@ -33,6 +40,13 @@ function Tweet(props): JSX.Element {
         <h1>{data.Name}</h1>
       </span>
       <p>{data.body}</p>
+      <div>
+        <span>
+          <button onClick={like}>
+            <Heart className=" h-6 w-6 " />
+          </button>
+        </span>
+      </div>
     </div>
   );
 }
--- pages/home/[id].tsx
+++ pages/home/[id].tsx
@@ -6,14 +6,13 @@ import Layout from "../../components/layout";
 import { isLoggedIn } from "../../lib/usersHelper";
 import Home from "../../components/HomeLayout";
 import { redirect } from "next/dist/server/api-utils";
-import { NOTFOUND } from "dns";
 import { useRouter } from "next/router";
 export const UserPage = ({ user }: { user: User }) => {
   const router = useRouter();
   const handleClick = async () => {
     if (!isLoggedIn()) {
       // Prompt for log in
-      
+
       return;
     }
     const resp = await fetch("/api/follow", {
