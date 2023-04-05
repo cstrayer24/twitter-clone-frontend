5f906c1fd7587af9591bc14e02d6074ebfe63bb3
Tim <tim@laborem.com>
2023-04-05 10:03:40 -0400

    upload to s3

---
+++ lib/createImageUrl.ts
@@ -0,0 +1,2 @@
+export const buildImgUrl = (key: string) =>
+  `https://twitter-clone-bye-elon.s3.us-east-2.amazonaws.com/${key}`;
--- pages/api/pfp.ts
+++ pages/api/pfp.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { randomUUID } from "crypto";
 import prisma from "../../lib/prisma";
-import formidable from "formidable";
-import { writeFile } from "fs/promises";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const config = {
   api: {
@@ -9,14 +9,37 @@ export const config = {
   },
 };
 
+const s3Client = new S3Client({
+  region: "us-east-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
 async function pfp(req: NextApiRequest, res: NextApiResponse) {
   const chunks = [];
+  // check if user logged in
   req.on("data", (chunk) => chunks.push(chunk));
   req.on("end", async () => {
     const b = Buffer.concat(chunks);
-    const thing = await writeFile("image.jpg", b);
-    console.log(thing);
+    const key = randomUUID();
+    const command = new PutObjectCommand({
+      Key: key + ".jpeg",
+      Bucket: "twitter-clone-bye-elon",
+      Body: b,
+      ContentType: "jpeg",
+    });
+
+    try {
+      await s3Client.send(command);
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
+
+    // user.update({profileId: key})
+    return res.json({ key });
   });
-  return res.status(200).json({ test: "test" });
 }
 export default pfp;
