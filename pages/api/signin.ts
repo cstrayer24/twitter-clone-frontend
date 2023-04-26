e28f8fc2eca458e2bf6b0365a8b10b53b1b56d9b
cstrayer24 <cstrayer4303@gmail.com>
2023-04-25 22:25:54 -0400

    deploy4

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,43 +1,43 @@
-//third party
-import type { NextApiRequest, NextApiResponse } from "next";
-import * as argon2 from "argon2";
-//my own
-import prisma from "../../lib/prisma";
+// //third party
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import * as argon2 from "argon2";
+// //my own
+// import prisma from "../../lib/prisma";
 
-async function signin(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    return res.status(400).json("bad request");
-  }
-  const { email, password } = req.body;
+// async function signin(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method !== "POST") {
+//     return res.status(400).json("bad request");
+//   }
+//   const { email, password } = req.body;
 
-  const user = await prisma.user.findFirst({
-    where: { email },
-  });
+//   const user = await prisma.user.findFirst({
+//     where: { email },
+//   });
 
-  if (!user) {
-    return res.status(404).json({ error: "no user" });
-  }
+//   if (!user) {
+//     return res.status(404).json({ error: "no user" });
+//   }
 
-  const isValidPassword = await argon2.verify(user.password, password);
+//   const isValidPassword = await argon2.verify(user.password, password);
 
-  if (!isValidPassword) {
-    return res.status(400).json({ error: "wrong password" });
-  }
-  const expiresAt = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
-  // const session: any = await prisma.session.create({
-  //   data: {
-  //     userId: user.id,
-  //     expiresAt,
-  //   },
-  // });
-  // res.setHeader("Set-Cookie", [
-  //   `sessionId=${
-  //     session.id
-  //   }; Path=/; HttpOnly; Expires=${expiresAt.toUTCString()};`,
-  //   `isLoggedIn=${1}; tOrf=1; Path=/; Expires=${expiresAt.toUTCString()};`,
-  // ]);
+//   if (!isValidPassword) {
+//     return res.status(400).json({ error: "wrong password" });
+//   }
+//   const expiresAt = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+//   // const session: any = await prisma.session.create({
+//   //   data: {
+//   //     userId: user.id,
+//   //     expiresAt,
+//   //   },
+//   // });
+//   // res.setHeader("Set-Cookie", [
+//   //   `sessionId=${
+//   //     session.id
+//   //   }; Path=/; HttpOnly; Expires=${expiresAt.toUTCString()};`,
+//   //   `isLoggedIn=${1}; tOrf=1; Path=/; Expires=${expiresAt.toUTCString()};`,
+//   // ]);
 
-  res.send({ id: user.id, email: user.email, name: user.name });
-}
+//   res.send({ id: user.id, email: user.email, name: user.name });
+// }
 
-export default signin;
+// export default signin;
